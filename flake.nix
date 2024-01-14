5f242972aa8e144cd4b4c4b7918c79210a4aabc3
Wesley Jr. <dev@wesleyjrz.com>
2024-01-14 16:15:11 -0300

    feat: add flake for development

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "My personal blog made with Hugo.";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
